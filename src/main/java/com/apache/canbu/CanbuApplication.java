ee11e097eefeb8aa4af773a18eea3931c3a1fd84
dlalscjf94 <ssw01092@naver.com>
2022-05-30 00:35:30 +0900

    first commit canbu.com

---
+++ src/main/java/com/apache/canbu/CanbuApplication.java
@@ -0,0 +1,13 @@
+package com.apache.canbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanbuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanbuApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/apache/canbu/controller/homecontroller.java
@@ -0,0 +1,16 @@
+package com.apache.canbu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class homecontroller {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/home")
+    public String home() {
+
+        System.out.println("hompage");
+        return "home";
+    }
+}
